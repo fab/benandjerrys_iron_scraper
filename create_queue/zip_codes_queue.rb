ed262ceca697a12e3515806a9f90f121fe198783
Fab Mackojc <fab.mackojc@gmail.com>
2013-09-18 17:12:30 +1000

    Faster method to create zip codes message queue

--- create_queue/zip_codes_queue.rb
+++ create_queue/zip_codes_queue.rb
@@ -4,6 +4,8 @@ require 'csv'
 ironmq = IronMQ::Client.new(:token => params['token'], :project_id => params['project'])
 queue = ironmq.queue('zip_codes')
 csv = CSV.open('zip_codes.csv').map(&:first)
+messages = []
 csv.each do |zip_code|
-  queue.post(zip_code)
+  messages << {:body => zip_code}
 end
+queue.post(messages)
