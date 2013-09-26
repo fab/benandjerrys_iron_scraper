9ef594e74545511da3f5a977ef342bcbb2eefad1
Fab Mackojc <fab.mackojc@gmail.com>
2013-09-26 16:30:24 +1000

    Messages added to queue now expire after one month

--- create_queue/zip_codes_queue.rb
+++ create_queue/zip_codes_queue.rb
@@ -6,6 +6,6 @@ queue = ironmq.queue('zip_codes')
 csv = CSV.open('zip_codes.csv').map(&:first)
 messages = []
 csv.each do |zip_code|
-  messages << {:body => zip_code}
+  messages << {:body => zip_code, :expires_in => 2592000}
 end
 queue.post(messages)
